47074ad52aeb7ff368b85941c2de0d5698e7a755
longbui <buingohoanglong2000@gmail.com>
2020-05-27 09:45:45 +0700

    Add all my files

---
+++ lib/UserInfo.dart
@@ -0,0 +1,25 @@
+class UserInfo {
+  String _id;
+  String _name;
+  String _phone;
+  String _email;
+  String _password;
+
+  UserInfo({String id, String name, String phone, String email, String password}) {
+    _id = id;
+    _name = name;
+    _phone = phone;
+    _email = email;
+    _password = password;
+  }
+  void setID(String id) {_id = id;}
+  void setName(String name) {_name = name;}
+  void setPhone(String phone) {_phone = phone;}
+  void setEmail(String email) {_email = email;}
+  void setPassword(String password) {_password = password;}
+  String getID() {return _id;}
+  String getName() {return _name;}
+  String getPhone() {return _phone;}
+  String getEmail() {return _email;}
+  String getPassword() {return _password;}
+}
