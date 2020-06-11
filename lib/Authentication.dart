c64c14a1fce567f6f8bded3bc3de19900b524fc4
longbui <buingohoanglong2000@gmail.com>
2020-06-11 23:20:44 +0700

    new changes

--- lib/Authentication.dart
+++ lib/Authentication.dart
@@ -7,14 +7,14 @@ class Authentication {
   Future<http.Response> signUp(UserInfo info) async {
     var body = info.toJson();
     var header = {"Content-Type": "application/json"};
-    final url = 'https://bkconnect.serveo.net/register/';
+    final url = 'http://10.0.2.2:5000/register/';
     return await http.post(url,headers: header, body: body);
   }
 
   Future<http.Response> signIn(UserInfo info) async {
     var body = info.toJson();
     var header = {"Content-Type": "application/json"};
-    final url = 'https://bkconnect.serveo.net/login/';
+    final url = 'http://10.0.2.2:5000/login/';
     return await http.post(url,headers: header, body: body);
   }
 }
--- server/server.py
+++ server/server.py
@@ -1,61 +1,72 @@
 from flask import Flask,request,jsonify
 from InfoManager import InfoManager
-
+from flask_jwt_extended import JWTManager
+from flask_jwt_extended import (create_access_token, jwt_required, get_jwt_identity, get_raw_jwt)
+import datetime
 
 app = Flask(__name__)
+app.config['JWT_SECRET_KEY'] = 'this is a secret key'
+revoked_tokens = []
+
+jwt = JWTManager(app)
 
 infoManager = InfoManager()
+
+@app.route("/test/", methods=["GET"])
+def test():
+    return "Hello World"
 
 @app.route("/login/",methods=["POST"])
 def login():
     user = request.json
-    msg = {
-        "status": "failure",
-        "type": "invalid username or password",
-        "body": {}
-    }
 
     if user["username"] == None or user["password"] == None:
-        msg["type"] = "lost connection"
+        msg = {"status": "failure", "message": "incomplete login information"}
         return jsonify(msg)
     
     if infoManager.authorizeSignIn(username=user["username"], password=user["password"]):
-        info = infoManager.getUser(user["username"])
-        msg["status"] = "success"
-        msg["type"] = "login sucessfully"
-        msg["body"] = {
-            "username": info["username"],
-            "id": info["id"],
-            "email": info["email"],
-            "phone": info["phone"],
-            "password": ""            
+        token = create_access_token(identity=user["username"], expires_delta=datetime.timedelta(minutes=1.0))
+        msg = {
+            "status": "success",
+            "message": "loged in successfully",
+            "access_token": token,
         }
         return jsonify(msg)
- 
-    return jsonify(msg) 
+    else:
+        msg = {"status": "failure", "message": "invalid username or password"}
+        return jsonify(msg)
             
 
 @app.route("/register/", methods=["POST"])
 def register():
     user = request.json
-    msg = {
-        "status": "failure",
-        "type": "username already taken",
-    }
 
     if user["username"] == None or user["id"] == None or user["email"] == None or user["phone"] == None or user["password"] == None:
-        msg["type"] = "lost connection"
+        msg = {"status": "failure", "message": "incomplete register information"}
         return jsonify(msg)
 
     if infoManager.authorizeSignUp(username=user["username"]):
-        infoManager.addUser(user)
-        infoManager.printDB()
-        msg["status"] = "success"
-        msg["type"] = "register successfully"
+        msg = {"status": "success", "message": "registered information"}
+        return jsonify(msg)
+    else:
+        msg = {"status": "failure", "message": "username already taken"}
         return jsonify(msg)
 
+@app.route("/profile/", methods=["GET"])
+@jwt_required
+def send_profile():
+    username = get_jwt_identity()
+    user = infoManager.getUser(username=username)
+    msg = {
+        "username": user["username"],
+        "id": user["id"],
+        "email": user["email"],
+        "phone": user["phone"],          
+    }
     return jsonify(msg)
 
 
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True,host='localhost', port=5000)
